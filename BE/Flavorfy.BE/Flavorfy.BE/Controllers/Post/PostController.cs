d17e3299554dffb8d0962b396b5d168778a91877
vinhdn20 <vinhdn20@gmail.com>
2024-10-02 22:40:03 +0700

    update docker

---
+++ BE/Flavorfy.BE/Flavorfy.BE/Controllers/Post/PostController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Flavorfy.BE.Controllers.Post
+{
+    [Authorize]
+    public class PostController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost("post")]
+        public IActionResult AddPost()
+        {
+            try
+            {
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.ToString());
+                throw;
+            }
+        }
+    }
+}
---
+++ BE/Flavorfy.BE/Services/Services/PostService.cs
@@ -0,0 +1,23 @@
+﻿using Domain.Entities;
+using Repository.Interfaces;
+using Services.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services.Services
+{
+    public class PostService : BasicOperationService<Post>, IPostService
+    {
+        public PostService(IDBRepository repository) : base(repository)
+        {
+        }
+
+        public Task<Post> AddPostAsync(Post post)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
